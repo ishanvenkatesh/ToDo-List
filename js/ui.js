c27f934cced109aef419f9be7fee670a5ffc4209
Ishan Venkatesh <ishanv.official@gmail.com>
2026-07-21 16:38:20 -0700

    Add way to display projects

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,25 @@
 //All DOM manipulation
 
+const projectList = document.querySelector("#project-list");
+
+function renderProject(project) {
+    const projectItem = document.createElement("li");
+    projectItem.textContent = project.getName();
+    projectList.appendChild(projectItem);
+
+    const deleteProjectBtn = document.createElement("button");
+    deleteProjectBtn.textContent = "X";
+    projectItem.appendChild(deleteProjectBtn);
+}
+
 function renderProjects(projects) {
+    
+    projectList.replaceChildren();
+
+    for (const project of projects) {
+        renderProject(project)
+        
+    }
     
 }
 
