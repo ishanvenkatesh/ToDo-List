002699d39868ca5b3e06f15413d8cb5a976a7e2a
Ishan Venkatesh <ishanv.official@gmail.com>
2026-07-21 14:12:17 -0700

    Import all necessary modules and add event handlers for buttons

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { Project} from "./Project.js";
+import { Task} from "./Task.js";
+import { ProjectManager } from "./ProjectManager.js";
+import { renderProjects, renderTasks, showTaskDetails } from "./ui.js";
+
+const addProjectBtn = document.querySelector("#add-project-btn");
+const addTaskBtn = document.querySelector("#add-task-btn");
+
+addProjectBtn.addEventListener("click", (event) => {
+
+});
+
+addProjectBtn.addEventListener("click", (event) => {
+
+});
