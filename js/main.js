f6502eb239bb8a7a0065e2347becec4b274e54e8
Ishan Venkatesh <ishanv.official@gmail.com>
2026-07-21 14:42:50 -0700

    Add hidden form functionality to store projects

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-import { Project} from "./Project.js";
-import { Task} from "./Task.js";
+import { Project } from "./Project.js";
+import { Task } from "./Task.js";
 import { ProjectManager } from "./ProjectManager.js";
 import { renderProjects, renderTasks, showTaskDetails } from "./ui.js";
+
+const projectManager = new ProjectManager();
+projectManager.addProject(new Project("Default"));
 
 const addProjectBtn = document.querySelector("#add-project-btn");
 const addTaskBtn = document.querySelector("#add-task-btn");
 
-addProjectBtn.addEventListener("click", (event) => {
+const projectForm = document.querySelector("#project-form");
+const projectName = document.querySelector("#project-name");
 
+addProjectBtn.addEventListener("click", (event) => {
+});
+
+projectForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = projectName.value.trim();
+    if (!name) {return}
+    projectManager.addProject(new Project(name));
+    projectForm.reset();
+    projectForm.hidden = true;
+    renderProjects(projectManager.getProjects());
 });
 
 addProjectBtn.addEventListener("click", (event) => {
-
+    projectForm.hidden = false;
 });
