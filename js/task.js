9faee14f9cb5b8170bd498529b61274bd30f618b
Ishan Venkatesh <ishanv.official@gmail.com>
2026-07-21 13:24:45 -0700

    Add a way to check and modify completion

--- js/task.js
+++ js/task.js
@@ -1,9 +1,19 @@
 //Class for the todo item
 class Task {
+    #isComplete = false;
+
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+    }
+
+    getCompleteStatus() {
+        return this.#isComplete;
+    }
+
+    markComplete() {
+        #isComplete = true;
     }
 }
