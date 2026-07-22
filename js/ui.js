4892c117541c7e6c6ea0d8953fc998770c713d7e
Ishan Venkatesh <ishanv.official@gmail.com>
2026-07-21 18:52:36 -0700

    Implement add, remove task and display in neat list

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,7 @@
 //All DOM manipulation
 
 const projectList = document.querySelector("#project-list");
+const taskList = document.querySelector("#task-list");
 
 function renderProject(project, onDelete, onSelect) {
     const projectItem = document.createElement("li");
@@ -36,8 +37,46 @@ function renderProjects(projects, onDelete, onSelect) {
     
 }
 
-function renderTasks(project) {
+function renderTask(task, onDelete) {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = task.getTitle();
+    taskList.appendChild(taskItem);
 
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    taskItem.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", (event) => {
+        onDelete(task);
+    });
+
+    const taskInfo = document.createElement("ul");
+    taskItem.appendChild(taskInfo);
+
+    const taskDescription = document.createElement("li");
+    taskDescription.textContent = task.getDescription();
+    taskInfo.appendChild(taskDescription);
+
+    const taskDueDate = document.createElement("li");
+    taskDueDate.textContent = task.getDueDate();
+    taskInfo.appendChild(taskDueDate);
+
+    const taskPriority = document.createElement("li");
+    taskPriority.textContent = task.getPriority();
+    taskInfo.appendChild(taskPriority);
+
+    
+
+    
+
+}
+
+function renderTasks(tasks, onDelete) {
+    taskList.replaceChildren()
+
+    for (const task of tasks) {
+        renderTask(task, onDelete);
+    }
 }
 
 function showTaskDetails(task) {
